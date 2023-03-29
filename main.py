a568b6700cb70f9267ce472e498ec4b674659906
realayna <100434027+realayna@users.noreply.github.com>
2023-03-29 14:09:13 +0200

    small improvements

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from tkinter import *
+from gen_p import *
+
 import random
 import string 
+
 window = Tk()
 window.title("Password generator")
 window.config(padx=100, pady=100, bg="#E7E7E7")
 greeting = Label(text="Click to button to generate a password", bg="#E7E7E7", font=("Inter", 15))
 greeting.pack()
+pass_length = int(input())
 button1 = Button(text="CLICK HERE", bg="#E7E7E7", font=("Inter") )
 button1.pack()
+generate_password()
+
 window.mainloop()
