6f8e59ca233bf9994f326edca50c85e3155f698d
realayna <100434027+realayna@users.noreply.github.com>
2023-03-29 15:32:26 +0200

    meow

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ window.title("Password generator")
 window.config(padx=100, pady=100, bg="#E7E7E7")
 greeting = Label(text="Click to button to generate a password", bg="#E7E7E7", font=("Inter", 15))
 greeting.pack()
-pass_length = int(input())
-button1 = Button(text="CLICK HERE", bg="#E7E7E7", font=("Inter") )
+#pass_length = int(input())
+button1 = Button(text="CLICK HERE", bg="#E7E7E7", font=("Inter"), command=generate_password)
 button1.pack()
-generate_password()
+password_field = Entry(bg="#CFC1C1",
+                       font=("Inter", 15, "bold"), width=40)
+
+password_field.pack()
+#generate_password()
 
 window.mainloop()
